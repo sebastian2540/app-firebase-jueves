f852c3ee602504e31bb74b966d12bed74fcb8788
sebastian2540 <sebastian.villada.jaramillo@gmail.com>
2024-05-23 20:50:28 -0500

    [CONFIG] se realiza la configuración del listado de usuarios que están creados en la base de datos y se configura la funcionalidad de Borrar

--- src/components/pages/auth/ListadoUsuario.jsx
+++ src/components/pages/auth/ListadoUsuario.jsx
@@ -2,7 +2,7 @@ import Header from "../../helpers/Header";
 import "./ListadoUsuario.css";
 import React, { useEffect, useState } from "react";
 import { connDatabases } from "../../database/firebaseConfig";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, deleteDoc, doc, getDocs } from "firebase/firestore";
 import Swal from "sweetalert2";
 
 const ListadoUsuario = () => {
@@ -12,7 +12,7 @@ const ListadoUsuario = () => {
     let collectionUsuarios = collection(connDatabases, "usuario");
     let datosUsuario = await getDocs(collectionUsuarios);
     // console.log(collectionUsuarios);
-    setUsuario(datosUsuario.docs.map((doc) => ({ ...doc.data() })));
+    setUsuario(datosUsuario.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
     console.log(datosUsuario.docs.map((doc) => ({ ...doc.data() })));
   }
 
@@ -20,28 +20,56 @@ const ListadoUsuario = () => {
     getUsuarios();
   }, []);
 
+  function eliminarUsuario(id) {
+    Swal.fire({
+      title: "¿Está seguro?",
+      text: "¡No podrás revertir esto!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "¡Sí, bórralo!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        confirmar(id);
+        Swal.fire({
+          title: "Eliminado!",
+          text: "Su archivo ha sido eliminado.",
+          icon: "success",
+        });
+      }
+    });
+  }
+
+  async function confirmar(id) {
+    let deleteUser = doc(connDatabases, "usuario", id);
+    await deleteDoc(deleteUser);
+    getUsuarios()
+  }
+
   return (
     <section className="panel">
       <Header />
       <main className="panel-contenido">
-        {
-            usuarios.map(() => (
-                <section>
-                    <section>
-                        <p>Nombre: </p>
-                        <p>Usuario: </p>
-                        <p>Contraseña: </p>
-                        <p>Correo: </p>
-                        <p>Ciudad</p>
-                    </section>
+        {usuarios.map((element) => (
+          <section>
+            <section>
+              <p>ID: {element.id} </p>
+              <p>Nombre: {element.nombre}</p>
+              <p>Usuario: {element.user}</p>
+              <p>Contraseña: {element.password} </p>
+              <p>Correo: {element.email} </p>
+              <p>Ciudad: {element.ciudad} </p>
+            </section>
 
-                    <div>
-                        <button>Editar</button>
-                        <button>Eliminar</button>
-                    </div>
-                </section>
-            ))
-        }
+            <div>
+              <button>Editar</button>
+              <button onClick={() => eliminarUsuario(element.id)}>
+                Eliminar
+              </button>
+            </div>
+          </section>
+        ))}
       </main>
     </section>
   );
